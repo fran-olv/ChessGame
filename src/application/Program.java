76fe9104be56501a4154b502dba1953bcbec640c
Franciele B. de Oliveira <32960110+FranOlv9pt1br3uff@users.noreply.github.com>
2021-03-24 01:31:22 -0300

    Criacao das cores, criacao da visao do tabuleiro do usuario e criacao da partida

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,7 +1,6 @@
 package application;
 
-import boardgame.Board;
-import boardgame.Position;
+import chess.ChessMatch;
 
 public class Program {
 
@@ -10,7 +9,11 @@ public class Program {
 		//Position pos = new Position(3, 5);
 		//System.out.println(pos);
 		
-		Board board = new Board(8,8);
+		//Board board = new Board(8,8);
+		
+		ChessMatch chessMatch = new ChessMatch();
+		UI.printBoard(chessMatch.getPieces());
+		
 	}
 	
 }
