fda047533f633b37ec75cfb4e20b1c4e06897b41
Franciele B. de Oliveira <32960110+FranOlv9pt1br3uff@users.noreply.github.com>
2021-03-25 00:59:33 -0300

    Inclusao de mov possiveis de forma geral

--- src/chess/ChessMatch.java
+++ src/chess/ChessMatch.java
@@ -46,6 +46,9 @@ public class ChessMatch {
 		if(!board.thereIsAPiece(position)) {
 			throw new ChessException("There is no pice on source position");
 		}
+		if(!board.piece(position).isThereAnyPossibleMove()) {
+			throw new ChessException("There is no possibles moves for the chosen piecen");
+		}
 	}
 	
 	
