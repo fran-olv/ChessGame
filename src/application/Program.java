21bba4224baa32911d3fc40f99e474ce43c22a72
Franciele B. de Oliveira <32960110+FranOlv9pt1br3uff@users.noreply.github.com>
2021-03-25 00:29:31 -0300

    Movimento pecas

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,7 +1,10 @@
 package application;
 
-import boardgame.Position;
+import java.util.Scanner;
+
 import chess.ChessMatch;
+import chess.ChessPiece;
+import chess.ChessPosition;
 
 public class Program {
 
@@ -12,11 +15,20 @@ public class Program {
 		
 		//Board board = new Board(8,8);
 		
+		Scanner sc  = new Scanner(System.in);
 		ChessMatch chessMatch = new ChessMatch();
-		UI.printBoard(chessMatch.getPieces());
 		
-	}
-	
-
-	
+		while (true) {
+			UI.printBoard(chessMatch.getPieces());
+			System.out.println();
+			System.out.println("Source: ");
+			ChessPosition source = UI.readChessPosition(sc);
+			
+			System.out.println();
+			System.out.println("Target: ");
+			ChessPosition target = UI.readChessPosition(sc);
+			
+			ChessPiece capturedPiece = chessMatch.perfomChessMove(source, target);
+		}	
+	}	
 }
