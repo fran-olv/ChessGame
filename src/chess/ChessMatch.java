0eec03921965ef9ec154ff5b10134a51236403b9
Franciele B. de Oliveira <francielebatista@id.uff.br>
2021-03-24 22:11:43 -0300

    Criacao da conversao das posicoes das pecas de matricial pro padrao do xadrex

--- src/chess/ChessMatch.java
+++ src/chess/ChessMatch.java
@@ -1,7 +1,6 @@
 package chess;
 
 import boardgame.Board;
-import boardgame.Position;
 import chess.pieces.King;
 import chess.pieces.Rook;
 
@@ -24,12 +23,25 @@ public class ChessMatch {
 		}
 		return mat; 
 	}
+	// passando as posicioes de acordo com xadrez
+	private void placeNewPiece(char column, int row, ChessPiece piece) {
+		board.placePiece(piece, new ChessPosition(column, row).toPosition());
+		
+	}
+	
+	
 	
 	//Coloca pecas no tabuleiro para iniciar uma partida
-	private void InitialSetup() {
-		board.placePiece(new Rook(board,Color.WHITE),new Position(2,9));
+	private void InitialSetup() { 
+		placeNewPiece('b', 6,new Rook(board,Color.WHITE));
+		placeNewPiece('e', 8,new King(board,Color.BLACK));
+		placeNewPiece('e', 1,new King(board,Color.WHITE));
+		
+		
+		//posicoes no sistema de matriz
+		/*board.placePiece(new Rook(board,Color.WHITE),new Position(2,1));
 		board.placePiece(new King(board,Color.BLACK),new Position(0,4));
-		board.placePiece(new King(board,Color.WHITE),new Position(7,4));
+		board.placePiece(new King(board,Color.WHITE),new Position(7,4)); */
 	}
 	
 }
