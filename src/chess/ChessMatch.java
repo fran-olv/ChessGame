7652d46b98c8d1d443a8a38833c1da3963885d84
alynenobre <alynenobre@id.uff.br>
2021-04-11 12:12:59 -0300

    Update ChessMatch.java

--- src/chess/ChessMatch.java
+++ src/chess/ChessMatch.java
@@ -9,6 +9,7 @@ import java.util.List;
 import boardgame.Board;
 import boardgame.Piece;
 import boardgame.Position;
+import chess.pieces.Bishop;
 import chess.pieces.King;
 import chess.pieces.Pawn;
 import chess.pieces.Rook;
